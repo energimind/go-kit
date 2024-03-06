979199849d52624eab31188a26039fa8cb4718b4
Alan Bunjevac <alan.bunjevac@gmail.com>
2024-03-06 15:06:16 +0100

    Host address utilities

---
+++ hostutil/doc.go
@@ -0,0 +1,9 @@
+// Package hostutil provides utilities for working with host addresses.
+//
+// It includes functions for composing host addresses from specified addresses,
+// ports, and default ports. The package supports various formats for the address
+// and inserts a default port if one is not specified in the address.
+//
+// The ComposeAddress function composes a single host address, while the
+// ComposeAddresses function composes multiple host addresses.
+package hostutil
