d90633e4735bb3a68e7a7a4c10dbe5555f809740
Alan Bunjevac <alan.bunjevac@gmail.com>
2024-04-03 10:20:44 +0200

    ID generator collection module docs

---
+++ idgen/doc.go
@@ -0,0 +1,5 @@
+// Package idgen provides a collection of ID generators.
+//
+// Each generator in this package provides a unique way of generating IDs.
+// Currently, it includes generators based on UUIDs, condensed UUIDs, and short IDs.
+package idgen
