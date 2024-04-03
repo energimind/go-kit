9db0e114dc927c599f668ddb0a82a4f9d02e0a2c
Alan Bunjevac <alan.bunjevac@gmail.com>
2024-04-03 10:18:21 +0200

    UUID generator

---
+++ idgen/uuid/generator_test.go
@@ -0,0 +1,24 @@
+package uuid
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerator_GenerateID(t *testing.T) {
+	g := NewGenerator()
+
+	generated := make(map[string]struct{})
+
+	for range 200 {
+		id := g.GenerateID()
+
+		_, found := generated[id]
+		if found {
+			require.Fail(t, "duplicated ID", id)
+		}
+
+		generated[id] = struct{}{}
+	}
+}
